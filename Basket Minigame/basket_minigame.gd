ece88b6c20d45478b7f09879642c306de85e03f1
FlapjackIsTired <115042675+FlapjackIsTired@users.noreply.github.com>
2025-11-27 17:24:00 -0500

    Basket Minigame pt2
    Added a new fruit, and a system to randomly drop fruit. Theres a curve to control how they drop.

--- Basket Minigame/basket_minigame.gd
+++ Basket Minigame/basket_minigame.gd
@@ -1,19 +1,40 @@
 extends Node2D
 
-@onready var label: Label = $Label
+@onready var point_label: Label = $"Point Label"
+@onready var time_label: Label = $"Time Label"
+@onready var total_timer: Timer = $"Total Timer"
+@onready var micro_timer: Timer = $"Micro Timer"
 
+@export var curve : Curve
+
+var items = ["res://Basket Minigame/apple.tscn", 
+"res://Basket Minigame/banana.tscn"]
 var total_points = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var count = 0
+	for i in items:
+		items[count] = load(i)
+		count += 1
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text = str(total_points)
-
+	point_label.text = str(total_points)
+	time_label.text = str(total_timer.time_left)
+	micro_timer.wait_time = curve.sample(total_timer.time_left)
+	print(micro_timer.wait_time)
 
 func _on_basket_player_points_collected(points) -> void:
 	total_points += points
+
+
+func _on_micro_timer_timeout() -> void:
+	#Gonna design it so when we are 15 seconds in, it will drop one item
+	#30 seconds in, it will drop two
+	#45, it will drop three
+	var item = items[randi_range(0,1)].instantiate()
+	add_child(item)
+	item.position = Vector2(randf_range(-160, 160), -90)
