0dd2099907a5d2d8c5683537d84d3e807409698d
FlapjackIsTired <115042675+FlapjackIsTired@users.noreply.github.com>
2025-12-16 19:57:33 -0500

    Zapper game
    Did some stuff to flesh out the game. There are a bunch of bugs but ive been coding for like 3 hours so ill just try again tomorrow

--- Zapper Minigame/cowboy.gd
+++ Zapper Minigame/cowboy.gd
@@ -1,28 +1,43 @@
 extends Area2D
 
+signal spot_shared
+
 @onready var death_timer: Timer = $"Death Timer"
 
 @export var stats : Resource
 @export var curve : Curve
 
 var points
+var is_solo = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#death_timer.wait_time = stats.survival_time
 	points = stats.value
 	curve.max_domain = death_timer.wait_time
 	death_timer.start(stats.survival_time)
-	#curve.get_point_position(2).x = death_timer.wait_time
+	
+	#curve.get_point_position(3).x = death_timer.wait_time
+	#curve.add_point(Vector2(death_timer.wait_time/2, 0.75))
+	#curve.add_point(Vector2(death_timer.wait_time, 0))
+	#print(Vector2(death_timer.wait_time/2, 0.75))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
 	#var time_left_flipped = death_timer.wait_time - death_timer.time_left
 	
-	scale = Vector2(curve.sample(death_timer.wait_time - death_timer.time_left), 
-	curve.sample(death_timer.wait_time - death_timer.time_left))
+	#scale = Vector2(curve.sample(death_timer.wait_time - death_timer.time_left),
+	#curve.sample(death_timer.wait_time - death_timer.time_left))
+	
+	#print(curve.sample(death_timer.wait_time - death_timer.time_left))
 
 
 func _on_death_timer_timeout() -> void:
-	print("dead")
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	is_solo = false
+	spot_shared.emit(get_node("."), get_overlapping_areas()[0])
--- Zapper Minigame/zapper_minigame.gd
+++ Zapper Minigame/zapper_minigame.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+
+
 @onready var micro_timer: Timer = $"Micro Timer"
 @onready var total_timer: Timer = $"Total Timer"
+@onready var time_label: Label = $"Time Label"
+@onready var score_label: Label = $"Score Label"
 @onready var spawn_locations = [$"Top Row/Spot 1",
 $"Top Row/Spot 2", 
 $"Top Row/Spot 3",
@@ -18,6 +22,11 @@ $"Bottom Row/Spot 12"]
 @export var curve : Curve
 
 var items = ["res://Zapper Minigame/cowboy.tscn"]
+var total_points = 0
+var current_items = []
+
+var times_called = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print(items)
@@ -29,36 +38,63 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	micro_timer.wait_time = curve.sample(total_timer.time_left)
+	time_label.text = str(int(total_timer.time_left))
 
 
 func _on_micro_timer_timeout() -> void:
 	var amount = 1
 	var count = 0
-	if total_timer.time_left <= 50:
+	
+	if total_timer.time_left <= 50 and total_timer.time_left >= 35:
 		amount = 2
-	elif total_timer.time_left <= 40:
+	elif total_timer.time_left <= 35 and total_timer.time_left >= 20:
 		amount = 3
-	elif total_timer.time_left <= 30:
+	elif total_timer.time_left <= 20 and total_timer.time_left >= 0:
 		amount = 5
-	elif total_timer.time_left <= 20:
-		amount = 6
-	
-	var current_items = []
-	
+
 	while count < amount:
-		var item = items[0].instantiate()
-		var spawn_spot = spawn_locations[randi_range(0,11)].global_position
-		current_items.append(item)
-		
-		for i in current_items:
-			if spawn_spot == i.global_position:
-				spawn_spot = spawn_locations[randi_range(0,11)].global_position
-		
-		add_child(item)
-		
-		item.position = spawn_spot
+		spawn_item()
 		count += 1
+		#bug is caused bc is_solo is false for somereason. will fix tom
+
 
 
 func _on_total_timer_timeout() -> void:
 	pass
+
+
+func _on_cursor_shot(points) -> void:
+	total_points += points
+	score_label.text = str(total_points)
+
+func fix_position(item1, item2):
+	times_called += 1
+	if times_called == 2:
+		var old_position = item2.global_position
+		if current_items.size() <= 12:
+			while true:
+				if old_position == item2.global_position:
+					item2.global_position = spawn_locations[randi_range(0,11)].global_position
+				else:
+					item2.is_solo = true
+					item1.is_solo = true
+					break
+			times_called = 0
+		
+		else:
+			item2.queue_free()
+		
+
+func spawn_item():
+	print("go")
+	if current_items.size() <= 12:
+		var item = items[0].instantiate()
+		var spawn_spot = spawn_locations[randi_range(0,11)].global_position
+			
+		item.spot_shared.connect(fix_position)
+		current_items.append(item)
+		if item.is_solo == true:
+			print("spawned")
+			item.position = spawn_spot
+			add_child(item)
+			
